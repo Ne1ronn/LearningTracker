371dd59e14ca4db9cca6f59fdf65bed7fa8dcb96
Yesset <esetkozcahmet87@gmail.com>
2025-10-19 03:32:48 +0600

    test telegram bot handlers

--- telegram_bot/dispatcher.py
+++ telegram_bot/dispatcher.py
@@ -1,11 +1,11 @@
 from aiogram import Bot, Dispatcher
 import asyncio
-from routers.test import my_router
+from routers.test import router
 
 async def main():
     bot = Bot(token="8413546619:AAEDhdhZuKjsTCleBs8P5L9QA4_EeNmWtHI")
     dp = Dispatcher()
-    dp.include_router(my_router)
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
--- telegram_bot/routers/test.py
+++ telegram_bot/routers/test.py
@@ -2,8 +2,24 @@ from aiogram import Router, types
 from aiogram.filters import Command
 from typing import Any
 
-my_router = Router(name=__name__)
+router = Router(name=__name__)
 
-@my_router.message(Command("start"))
+@router.message(Command("start"))
 async def start_handler(message: types.Message) -> Any:
     await message.answer('Hello from my router!')
+
+@router.message(Command("Add entry"))
+async def message_handler(message: types.Message) -> Any:
+    await message.answer("Give me title of new entry")
+    await title_handler()
+
+async def is_string(message: types.Message) -> bool:
+    return isinstance(message.text, str)
+
+@router.message()
+async def title_handler(message: types.Message) -> Any:
+    if not is_string(message):
+        await message.answer("This isn't a string")
+    else:
+        await message.answer("Now description of new entry")
+
