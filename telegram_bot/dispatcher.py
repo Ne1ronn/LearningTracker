7bcd3f7f8b2068adbca006fd2776410da0a69983
Yesset <esetkozcahmet87@gmail.com>
2025-10-24 22:43:39 +0600

    creat a database setup handler

--- telegram_bot/dispatcher.py
+++ telegram_bot/dispatcher.py
@@ -2,12 +2,14 @@ from aiogram import Bot, Dispatcher
 import asyncio
 from routers.entry_add import router as add_router
 from routers.entry_get import router as get_router
+from routers.database_bot import router as db_router
 
 async def main():
     bot = Bot(token="8413546619:AAEDhdhZuKjsTCleBs8P5L9QA4_EeNmWtHI")
     dp = Dispatcher()
     dp.include_router(add_router)
     dp.include_router(get_router)
+    dp.include_router(db_router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
---
+++ telegram_bot/routers/database_bot.py
@@ -0,0 +1,18 @@
+from aiogram import Router, types
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+import httpx
+
+router = Router()
+API_URL = "http://127.0.0.1:8000/setup_database"
+
+@router.message(Command("setup"))
+async def setup_database(message: types.Message):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(API_URL)
+
+    if response.status_code == 200:
+        await message.answer("Database created successfully✅")
+    else:
+        await message.answer(f"Error:{response.text}")
