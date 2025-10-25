5f84431184e1d2fb51b4fe02291f878dc8f93a75
Yesset <esetkozcahmet87@gmail.com>
2025-10-25 23:27:22 +0600

    creat a delete bot handler for entry table

--- telegram_bot/dispatcher.py
+++ telegram_bot/dispatcher.py
@@ -4,6 +4,7 @@ from routers.entry_add import router as add_entry_router
 from routers.entry_get import router as get_entry_router
 from routers.database_bot import router as db_router
 from routers.entry_update import router as update_entry_router
+from routers.entry_delete import router as delete_entry_router
 
 async def main():
     bot = Bot(token="8413546619:AAEDhdhZuKjsTCleBs8P5L9QA4_EeNmWtHI")
@@ -11,6 +12,7 @@ async def main():
     dp.include_router(add_entry_router)
     dp.include_router(get_entry_router)
     dp.include_router(update_entry_router)
+    dp.include_router(delete_entry_router)
     dp.include_router(db_router)
     await dp.start_polling(bot)
 
---
+++ telegram_bot/routers/entry_delete.py
@@ -0,0 +1,36 @@
+from aiogram import Router, types
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import Command
+import httpx
+
+router = Router()
+API_URL = "http://127.0.0.1:8000/entries/{entry_id}"
+
+class DeleteEntryState(StatesGroup):
+    waiting_id = State()
+
+@router.message(Command("delete_entry"))
+async def get_id(message: types.Message, state: FSMContext):
+    await message.answer("Enter the id of entry:")
+    await state.set_state(DeleteEntryState.waiting_id)
+
+@router.message(DeleteEntryState.waiting_id)
+async def delete_entry(message: types.Message, state: FSMContext):
+    try:
+        entry_id = int(message.text)
+        if not entry_id.is_integer():
+            raise ValueError
+    except ValueError:
+        await message.answer("Enter a integer number")
+        return
+
+    async with httpx.AsyncClient() as client:
+        response = await client.delete(API_URL.format(entry_id=entry_id))
+
+    if response.status_code == 200:
+        await message.answer("Entry deleted successfully ✅")
+    else:
+        await message.answer("Entered a wrong id, try again ❌")
+        return
+    await state.clear()
